eff697f7ed0318d9ca550f2e35f667a100c5e106
kris1803 <kristiansstraume17@gmail.com>
2022-06-30 00:49:22 +0200

    added react ga

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,9 +7,22 @@ import { useEffect } from 'react';
 function MyApp({ Component, pageProps }: AppProps) {
 
   useEffect(() => {
-    const ReactGA = import('react-ga4');
-    
+    (async () => {
+    const ReactGA = (await import('react-ga4')).default;
+    const TRACKING_ID = "G-HE237YC5RK"; // YOUR_OWN_TRACKING_ID
+    ReactGA.initialize(TRACKING_ID, {});
+    ReactGA.send({ hitType: 'pageview', page: window.location.pathname + window.location.search });
+    reportWebVitals(({ id, name, value }: { id:any, name:any, value: any }) =>
+      ReactGA.event({
+        action: name,
+        category: 'Web Vitals',
+        label: id,
+        nonInteraction: true,
+        value: typeof value === 'number'? Math.round(value) : value,
+      })
+    );
 
+    })()
     // @ts-ignore
     //import("bootstrap/dist/js/bootstrap.min.js");
   });
@@ -17,4 +30,16 @@ function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
 
-export default MyApp
+const reportWebVitals = (onPerfEntry: any) => {
+  if (onPerfEntry && onPerfEntry instanceof Function) {
+    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
+      getCLS(onPerfEntry);
+      getFID(onPerfEntry);
+      getFCP(onPerfEntry);
+      getLCP(onPerfEntry);
+      getTTFB(onPerfEntry);
+    });
+  }
+};
+
+export default MyApp;
